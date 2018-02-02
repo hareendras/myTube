dd39058e6bc91a68cee4e694cd314e8fd1e00f99
Hareendra Seneviratne <hareendras@millenniumitesp.com>
2018-02-02 14:18:14 +0530

    Key removed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-import API_KEY from './keys';
+import API_KEY from '../keys';
 
 
 
--- src/keys.js
+++
@@ -1,2 +0,0 @@
-const API_KEY = 'AIzaSyBPKb4a1IEZf7n2e8SK_xZ4R10x3sJtUH4';
-export default API_KEY;
